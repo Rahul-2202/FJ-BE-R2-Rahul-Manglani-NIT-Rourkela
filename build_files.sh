8cddd2a500d7d16ef3b2f71c0ef35318ac612eba
Rahul Manglani <80082925+Rahul-2202@users.noreply.github.com>
2023-09-15 21:06:21 +0530

    setup changes

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 echo "Building files"
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput --clear
 echo "Build END"
